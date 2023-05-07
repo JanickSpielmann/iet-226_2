6bbc53bed2693ea58c87fe5fed87082310eaab4b
Janick Spielmann <janick.spielmann@messageme.ch>
2023-05-07 17:43:43 +0200

    some stuff

--- src/main/java/ch/gibb/firma/Personen.java
+++ src/main/java/ch/gibb/firma/Personen.java
@@ -10,7 +10,7 @@ public class Personen {
         f.mittarbeiterHinzufugen(new Chef("Müller", "Martin", 156, "Logistik"));
         f.mittarbeiterHinzufugen(new Lehrling("Nica", "Luca", 924, 4));
         f.mittarbeiterHinzufugen(new Lehrling("Flück", "Marcel", 100, 3));
-        f.mittarbeiterHinzufugen(new Fachangestellte("Gates","Bill",230,new Chef("Meier","Jan",130,"Wartung")));
+        f.mittarbeiterHinzufugen(new Fachangestellte("Gates","Bill",230, (Chef) f.getMitarbeiter(0)));
         for (Person person: f.getMitarbeiter()) {
             person.print();
             System.out.println(person.berechneFerien(55));
